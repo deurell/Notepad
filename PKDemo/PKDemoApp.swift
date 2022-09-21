94e85d157ba3dde60264b6dbab98285db61c0fdc
Mikael Deurell <mikael@deurell.net>
2022-09-21 13:55:24 +0200

    Initial Commit

---
+++ PKDemo/PKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKDemoApp.swift
+//  PKDemo
+//
+//  Created by Mikael Deurell on 2022-09-21.
+//
+
+import SwiftUI
+
+@main
+struct PKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
